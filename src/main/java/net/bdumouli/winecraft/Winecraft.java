4bf78e5f602b116a099d498505fe487ddd3ecef9
Baptiste Dumoulin <Baptiste.Dumoulin@insa-rennes.fr>
2023-11-07 20:16:19 +0200

    Initial commit

---
+++ src/main/java/net/bdumouli/winecraft/Winecraft.java
@@ -0,0 +1,17 @@
+package net.bdumouli.winecraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Winecraft implements ModInitializer {
+    public static final String MOD_ID = "winecraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/bdumouli/winecraft/WinecraftClient.java
@@ -0,0 +1,9 @@
+package net.bdumouli.winecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+public class WinecraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
